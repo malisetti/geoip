2a44a03beb4b429226d625373a6891b9a28bbf3b
Seshachalam Malisetti <seshachalam.malisetti@imaginea.com>
2016-07-08 12:45:07 +0530

    changes

--- main.go
+++ main.go
@@ -10,19 +10,20 @@ import (
 )
 
 type GeoData struct {
-	City      string  `json: "city"`
-	Country   string  `json: "country"`
-	IsoCode   string  `json: "country_code"`
-	Latitude  float64 `json: "latitude"`
-	Longitude float64 `json: "longitude"`
-	TimeZone  string  `json: "time_zone"`
-	IP        string  `json: "ip"`
-	Status    string  `json: "status"`
+	City      string  `json:"city"`
+	Country   string  `json:"country"`
+	IsoCode   string  `json:"country_code"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	TimeZone  string  `json:"time_zone"`
+	IP        string  `json:"ip"`
+	Status    string  `json:"status"`
 }
 
 type GeoDataErr struct {
-	Status  string `json: "status"`
-	Message string `json: "message"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	IP string `json:"ip"`
 }
 
 func main() {
@@ -32,6 +33,9 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	defer db.Close()
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
@@ -39,14 +43,14 @@ func main() {
 	router.GET("/ip/:ip", func(c *gin.Context) {
 		var successResponce GeoData
 		var errResponse GeoDataErr
-		db, err := geoip2.Open("GeoLite2-City.mmdb")
+
 		if err != nil {
 			errResponse.Message = err.Error()
 			errResponse.Status = "fail"
+			errResponse.IP = ""
 			c.JSON(500, errResponse)
 			return
 		}
-		defer db.Close()
 
 		passedIP := c.Param("ip")
 
@@ -62,6 +66,7 @@ func main() {
 		if err != nil {
 			errResponse.Message = err.Error()
 			errResponse.Status = "fail"
+			errResponse.IP = clientIP
 			c.JSON(500, errResponse)
 			return
 		}
