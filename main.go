e2510003d80d79d6663a0458a48e0fb8c78b9d38
Seshachalam Malisetti <seshachalam.malisetti@imaginea.com>
2016-09-26 18:08:30 +0530

    move between the db files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/fasthttp"
 	"github.com/oschwald/geoip2-golang"
@@ -8,25 +9,34 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/middleware"
 )
 
+var db *geoip2.Reader
+
 type geoData struct {
-	City      string  `json:"city"`
-	Country   string  `json:"country"`
-	IsoCode   string  `json:"country_code"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	TimeZone  string  `json:"time_zone"`
-	IP        string  `json:"ip"`
-	Status    string  `json:"status"`
+	City      string      `json:"city"`
+	Country   string      `json:"country"`
+	IsoCode   string      `json:"country_code"`
+	Latitude  float64     `json:"latitude"`
+	Longitude float64     `json:"longitude"`
+	TimeZone  string      `json:"time_zone"`
+	IP        string      `json:"ip"`
+	Status    string      `json:"status"`
+	Err       *geoDataErr `json:"error"`
 }
 
 type geoDataErr struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 	IP      string `json:"ip"`
+}
+
+type localChanData struct {
+	ip    string
+	ipout chan *geoData
 }
 
 func main() {
@@ -41,24 +51,50 @@ func main() {
 		mmdbPath = "GeoLite2-City.mmdb"
 	}
 
-	db, err := geoip2.Open(mmdbPath)
-	defer db.Close()
+	var openerr error
+	db, openerr = geoip2.Open(mmdbPath)
+	checkErr(openerr)
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+
+			curTime := time.Now().UTC()
+			month := fmt.Sprintf("%02d", curTime.Month())
+			newfilepath := "GeoLite2-City-" + month + ".mmdb"
+			firstTues := firstTuesday(curTime.Year(), curTime.Month())
+			if firstTues == curTime.Day() {
+				if _, err := os.Stat(newfilepath); err == nil {
+					db.Close()
+					//exists, so open the new one
+					println("opening the new one")
+					db, openerr = geoip2.Open(newfilepath)
+					checkErr(openerr)
+				}
+			}
+		}
+	}()
+
+	ipin := make(chan localChanData)
+	defer close(ipin)
+
+	for i := 0; i < 300; i++ {
+		go func(ipin chan localChanData) {
+			for {
+				select {
+				case input := <-ipin:
+					record := queryDB(input.ip)
+					input.ipout <- record
+				}
+			}
+		}(ipin)
+	}
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/ip/:ip", func(c echo.Context) error {
-		var successResponce geoData
-		var errResponse geoDataErr
-
-		if err != nil {
-			errResponse.Message = err.Error()
-			errResponse.Status = "fail"
-			errResponse.IP = ""
-			return c.JSON(http.StatusInternalServerError, errResponse)
-		}
-
 		passedIP := c.Param("ip")
 
 		var clientIP string
@@ -67,28 +103,55 @@ func main() {
 		} else {
 			clientIP = c.Request().RealIP()
 		}
+		data := make(chan *geoData)
+		defer close(data)
+		ipin <- localChanData{clientIP, data}
+		for {
+			select {
+			case output := <-data:
+				if output.Err != nil {
+					return c.JSON(http.StatusNotFound, output.Err)
+				}
 
-		ip := net.ParseIP(clientIP)
-		record, err := db.City(ip)
-		if err != nil {
-			errResponse.Message = err.Error()
-			errResponse.Status = "fail"
-			errResponse.IP = clientIP
-			return c.JSON(http.StatusNotFound, errResponse)
+				return c.JSON(http.StatusOK, output)
+			}
 		}
-		successResponce.City = record.City.Names["en"]
-		successResponce.Country = record.Country.Names["en"]
-		successResponce.IsoCode = record.Country.IsoCode
-		successResponce.Latitude = record.Location.Latitude
-		successResponce.Longitude = record.Location.Longitude
-		successResponce.TimeZone = record.Location.TimeZone
-		successResponce.IP = clientIP
-		successResponce.Status = "success"
-
-		return c.JSON(http.StatusOK, successResponce)
 	})
 
+	e.Logger().Debug("Starting on port " + port)
 	e.Run(fasthttp.New(":" + port))
+}
+
+func queryDB(strip string) *geoData {
+	ip := net.ParseIP(strip)
+	record, err := db.City(ip)
+	var data geoData
+	if err != nil {
+		var geoerr geoDataErr
+		geoerr.IP = strip
+		geoerr.Message = err.Error()
+		geoerr.Status = "fail"
+		data.Err = &geoerr
+
+		return &data
+	}
+	data.City = record.City.Names["en"]
+	data.Country = record.Country.Names["en"]
+	data.IsoCode = record.Country.IsoCode
+	data.Latitude = record.Location.Latitude
+	data.Longitude = record.Location.Longitude
+	data.TimeZone = record.Location.TimeZone
+	data.IP = strip
+	data.Status = "success"
+
+	return &data
+}
+
+// firstTuesday returns the day of the first Tuesday in the given month.
+func firstTuesday(year int, month time.Month) int {
+	t := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+
+	return (8-int(t.Weekday()))%7 + 2
 }
 
 func checkErr(err error) {
